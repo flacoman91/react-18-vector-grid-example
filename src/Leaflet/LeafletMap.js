9bf4a456c175a43b11f2f09229bcc165b6c8847b
Richard Dinh <richard.dinh@cfpb.gov>
2022-09-29 19:58:33 -0400

    adding marker and circle marker examples

--- src/Leaflet/LeafletMap.js
+++ src/Leaflet/LeafletMap.js
@@ -1,7 +1,16 @@
 import './LeafletMap.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import {
+    CircleMarker,
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    Tooltip
+} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { VectorGrid } from './VectorGrid';
+
+
 
 export const LeafletMap = () => {
   const center = [2.935403, 101.448205];
@@ -20,7 +29,23 @@ export const LeafletMap = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-          <VectorGrid />
+        <VectorGrid />
+          {/*
+          borrowed examples from
+          https://react-leaflet.js.org/docs/example-tooltips/
+          */}
+          <Marker position={center}>
+              <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+          </Marker>
+
+          <CircleMarker
+              center={[4.935403, 101.448205]}
+              pathOptions={{ color: 'red' }}
+              radius={20}>
+              <Tooltip>Tooltip for CircleMarker</Tooltip>
+          </CircleMarker>
       </MapContainer>
     </div>
   );
